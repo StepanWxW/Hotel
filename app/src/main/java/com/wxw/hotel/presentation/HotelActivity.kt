804421309d3d6c3d84dfb73f3b122259864e7f5a
Polyakov Stepan <stepan@bubuka.info>
2023-09-04 17:53:11 +0700

    work on hotelActivity2

--- app/src/main/java/com/wxw/hotel/presentation/HotelActivity.kt
+++ app/src/main/java/com/wxw/hotel/presentation/HotelActivity.kt
@@ -1,11 +1,17 @@
 package com.wxw.hotel.presentation
 
+
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 import com.wxw.hotel.R
 
@@ -13,6 +19,9 @@ import com.wxw.hotel.R
 class HotelActivity : AppCompatActivity() {
     private lateinit var viewModel: HotelViewModel
 
+    private lateinit var recyclerView: RecyclerView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel)
@@ -20,13 +29,31 @@ class HotelActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[HotelViewModel::class.java]
         val viewPager2 = findViewById<ViewPager2>(R.id.viewPager)
         val dotsIndicator = findViewById<DotsIndicator>(R.id.dotsIndicator)
-        val rating = findViewById<TextView>(R.id.textRating)
+        val textRating = findViewById<TextView>(R.id.textRating)
+        val placeButton = findViewById<Button>(R.id.placeButton)
+        val textPrice = findViewById<TextView>(R.id.textPrice)
+        val textViewDescription = findViewById<TextView>(R.id.textViewDescription)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        val layoutManager = FlexboxLayoutManager(this)
+        layoutManager.flexDirection = FlexDirection.ROW
+        layoutManager.flexWrap = FlexWrap.WRAP
+        recyclerView.layoutManager = layoutManager
 
         viewModel.hotelLive.observe(this){
-            val adapter = ImageAdapter(it.imageUrls)
-            viewPager2.adapter = adapter
+            val adapterImg = ImageAdapter(it.imageUrls)
+            viewPager2.adapter = adapterImg
             dotsIndicator.setViewPager2(viewPager2)
-            rating.text = "${it.rating} ${it.ratingName}"
+            textRating.text = "${it.rating} ${it.ratingName}"
+            placeButton.text = it.address
+            textPrice.text = "от "  + it.minimalPrice.toString() + " ₽"
+
+            val adapterPeculiarities = PeculiaritiesAdapter(it.aboutTheHotelEntity.peculiarities)
+            recyclerView.adapter = adapterPeculiarities
+            textViewDescription.text = it.aboutTheHotelEntity.description
         }
+
+
+
     }
 }
--- app/src/main/java/com/wxw/hotel/presentation/PeculiaritiesAdapter.kt
+++ app/src/main/java/com/wxw/hotel/presentation/PeculiaritiesAdapter.kt
@@ -3,30 +3,30 @@ package com.wxw.hotel.presentation
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
 import com.wxw.hotel.R
 
 
-class ImageAdapter(private val imageUrls: List<String>) :
-    RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+class PeculiaritiesAdapter(private val data: List<String>) :
+    RecyclerView.Adapter<PeculiaritiesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_peculiarities, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val imageUrl = imageUrls[position]
-        Picasso.get().load(imageUrl).into(holder.imageView)
+        val text = data[position]
+        holder.textView.text = text
     }
 
     override fun getItemCount(): Int {
-        return imageUrls.size
+        return data.size
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val textView: TextView = itemView.findViewById(R.id.textView)
     }
 }
